0191e42cb25a5d29c0bb3f4f81f72461af70f898
Camila Heinzmann <camilapostai.ch@gmail.com>
2021-05-09 22:47:33 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$("#staticform").submit(function (event) {
+  event.preventDefault();
+  $.ajax({
+    url: "https://api.staticforms.xyz/submit",
+    type: "POST",
+    dataType: "json",
+    data: $("#staticform").serialize(),
+    success: function (result) {
+      $("#staticform").each(function () {
+        this.reset();
+      });
+      $("#staticform p").html("Mensagem enviada com sucesso!");
+      setTimeout(function () {
+        $("#staticform p").html("");
+      }, 3000);
+    },
+    error: function (xhr, resp, text) {
+      $("#staticform p").html("Erro ao enviar mensagem!");
+      setTimeout(function () {
+        $("#staticform p").html("");
+      }, 3000);
+      console.log(xhr, resp, text);
+    },
+  });
+});
